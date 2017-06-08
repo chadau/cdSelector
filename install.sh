86779808566185749245f104544dd277913a94ff
Lucas Cheminade <lucas.cheminade@epitech.eu>
2017-06-08 10:03:41 +0200

    add make directory

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@
 ## Login   <lucas.cheminade@epitech.eu>
 ## 
 ## Started on  Thu Jun  8 01:06:20 2017 
-## Last update Thu Jun  8 01:12:54 2017 
+## Last update Thu Jun  8 10:02:48 2017 
 ##
 
 #################
 ###   INSTALL ###
 #################
 
+mkdir -p $HOME/bin
 cp ./cdselector $HOME/bin
 echo "alias cdselector='. cdselector'" >> $HOME/.bashrc
 echo "execute: ~/.bashrc"
